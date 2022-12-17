b93bd341600a6360ebcc41a69fdbec771c282979
divyashreevelu <116351058+divyashreevelu@users.noreply.github.com>
2022-12-17 17:18:59 +0530

    Add files via upload

---
+++ python practices2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  6 10:02:20 2022
+
+@author: Dell
+"""
+index = 1
+while index < 4:
+    index +=1
+    if index ==2 :
+        continue
+    print(index)
